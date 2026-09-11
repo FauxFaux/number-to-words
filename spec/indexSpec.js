0bad57c0797b17f5534e0ef621fa1cc43f6c5a7b
Chris West (Faux) <git@goeswhere.com>
2026-09-11 15:52:22 +0100

    chore: migrate to vitest

--- spec/indexSpec.js
+++ spec/indexSpec.js
@@ -4,12 +4,12 @@ var index = typeof require !== 'undefined' ? require('../src') : window.numberTo
 
 describe('index', function () {
     it('should expose a toOrdinal method', function () {
-        expect(index.toOrdinal).toEqual(jasmine.any(Function));
+        expect(index.toOrdinal).toEqual(expect.any(Function));
     });
     it('should expose a toWords method', function () {
-        expect(index.toWords).toEqual(jasmine.any(Function));
+        expect(index.toWords).toEqual(expect.any(Function));
     });
     it('should expose a toWordsOrdinal method', function () {
-        expect(index.toWordsOrdinal).toEqual(jasmine.any(Function));
+        expect(index.toWordsOrdinal).toEqual(expect.any(Function));
     });
 });
